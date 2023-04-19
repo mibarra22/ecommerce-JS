d407c2698b8a67a07c0769107ff1a4b74f76fba0
Miriam <miriam_lih@hotmail.com>
2023-04-19 00:09:05 -0500

    creando localStorage

--- js/main.js
+++ js/main.js
@@ -189,6 +189,7 @@ const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal =document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
 
 function cargarProductos(productosElegidos){
     contenedorProductos.innerHTML = "";
@@ -248,10 +249,30 @@ function actualizarBotonesAgregar(){
 
 const productosEnCarrito = [];
 
-function agregarAlCarrito(){}
+function agregarAlCarrito(e){
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
 
+    if(productosEnCarrito.some(producto => producto.id === idBoton)){
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
 
+    }else{
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
 
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+/*ACTUALIZAR NUMERITO */
+function actualizarNumerito(){
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+    
+}
 
 
 
